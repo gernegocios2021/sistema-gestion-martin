5a04dacfec1c5cc7a7b6b5972de2113221824803
German <gernegocios2021@gmail.com>
2026-08-05 13:14:24 -0300

    crear archivos panaderia y bar page

---
+++ app/modules/ventas/bar/page-bar.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function VentasBar() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">POS - Bar (En construcción)</h1>
+      <p className="text-gray-600 mt-4">Módulo de Punto de Venta para cafeterías y bares con barcode + botonera táctil.</p>
+    </div>
+  )
+}
